656eed4c244fac4cd4c9fc03a955155d8a9de285
Amanda Romero <mandicap@comcast.net>
2025-10-19 12:25:37 -0600

    chore: add phpcsfixer for folks who want the integration with their editor

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,4 @@ namespace Builtnoble\VitePHP\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
-}
+abstract class TestCase extends BaseTestCase {}
